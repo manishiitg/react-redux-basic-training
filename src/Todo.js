c001d345c1307d71d704d0bb3a3e250e401804b7
manish <manish@laptoplenovo>
2020-06-28 12:56:16 +0530

    connected with redux state amd actions

--- src/Todo.js
+++ src/Todo.js
@@ -1,6 +1,29 @@
-const Todo = (props) => {
+import { connect } from 'react-redux'
 
+import { addTodoAction } from "./redux/actions/todo"
+
+const Todo = (props) => {
     return (
         "Todo List"
     )
 }
+
+
+const mapStateToProps = (state) => {
+    return {
+        todo: state.todos
+    }
+}
+
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        addTodo: (payload) => {
+            dispatch(addTodoAction(...payload))
+        }
+    }
+}
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Todo)
