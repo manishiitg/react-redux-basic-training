9977101387ca593677814801169cb67e610eade4
manish <manish@laptoplenovo>
2020-06-29 09:44:57 +0530

    hooks with reselect

--- src/Todo.js
+++ src/Todo.js
@@ -1,17 +1,23 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, createContext } from 'react'
 
 import { connect } from 'react-redux'
 
 import { useDispatch, useSelector } from 'react-redux'
 
-
 import { addTodoAction } from "./redux/actions/todo"
 
+import { createSelector } from 'reselect'
+
+const getTodoList = createSelector(
+    state => state,
+    todos => todos.reverse()
+)
+
 function Todo(props) {
-    const todoList = useSelector(state => state)
+    const todoList = useSelector(getTodoList)
     const dispatch = useDispatch()
 
-    useEffect( () => {
+    useEffect(() => {
         console.log("re-render")
     })
 
